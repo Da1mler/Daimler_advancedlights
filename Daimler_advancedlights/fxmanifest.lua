746904f1e5d6d83db4d85c0b98c1ad6e7a4ff42b
Daimler <155020240+Da1mler@users.noreply.github.com>
2024-01-02 22:23:51 +0100

    Add files via upload

---
+++ Daimler_advancedlights/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Daimler'
+description 'Daimler Advanced Lights - Zapínání a vypínání dálkových světel když hráč zatroubí'
+version '1.0.0'
+
+client_scripts {
+    'server.lua'
+}
+
+dependencies {
+    'es_extended'
+}
